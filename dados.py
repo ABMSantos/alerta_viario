f1fd4d67faaa1f0cb3a5d628922a226b06a70007
anabarbara <aninhabarbara1997@hotmail.com>
2026-02-21 20:58:26 -0300

    first commit

---
+++ dados.py
@@ -0,0 +1,20 @@
+pontos_criticos = [
+    {
+        "nome": "Av. Presidente Vargas x Av. Independência",
+        "lat": -21.1767,
+        "lng": -47.8103,
+        "risco": "Alto índice de sinistros com motociclistas"
+    },
+    {
+        "nome": "Av. Dom Pedro I",
+        "lat": -21.1880,
+        "lng": -47.8200,
+        "risco": "Trecho com histórico de colisões traseiras"
+    },
+    {
+        "nome": "Anel Viário Sul",
+        "lat": -21.2100,
+        "lng": -47.8500,
+        "risco": "Alta velocidade e sinistros graves"
+    }
+]
